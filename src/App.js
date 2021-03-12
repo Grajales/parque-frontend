e999bd29dc1e5707022a154851d251cc548c5d9b
LG29878 <grajalesliliana@johndeere.com>
2021-03-12 13:22:30 -0500

    changed App.js and Index.js to read comment api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Link, Switch } from "react-router-dom";
+// import ArtistDetail from "./ArtistDetail.js";
+// import AllArtists from "./AllArtists.js";
+import axios from "axios";
+const backendUrl =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/api";
+// const backendUrl =
+//   process.env.REACT_APP_BACKEND_URL || "https://muse-backend.herokuapp.com/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      comments: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`${backendUrl}/artists`).then((response) => {
+      this.setState({
+        comments: response.data.comments,
+      });
+    });
+  }
+
+
+  render() {
+    console.log("comments",this.props.comments)
+    return (
+      <div className="App">
+        <nav>
+          <Link to="/">All Comments</Link>
+        </nav>
+        {/* <main>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={() => (
+                <AllArtists
+                  artists={this.state.artists}
+                  addArtist={this.addArtist}
+                />
+              )}
+            />
+            <Route
+              path="/artists/:id"
+              component={(routerProps) => (
+                <ArtistDetail
+                  {...routerProps}
+                  artists={this.state.artists}
+                  addSong={this.addSong}
+                />
+              )}
+            />
+          </Switch>
+        </main> */}
+      </div>
+    );
+  }
 }
 
 export default App;
