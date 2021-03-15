9f725e5d0edce1783f200ec1443d1518a2fd3252
LG29878 <grajalesliliana@johndeere.com>
2021-03-14 23:23:09 -0500

    started adding images to homepage

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link, Redirect } from 'react-router-dom';
+import PICNIC_IMAGE from "../../Images/HomePicnic.png";
+import PLAY_IMAGE from "../../Images/HomePlayGround.png";
+import CANNOE_IMAGE from "../../Images/HomeCannoeing.png";
+import HIKE_IMAGE from "../../Images/HomeRedBridge.jpg";
 class Home extends Component {
 render(){
         console.log("test home", this.props)
         return(
-            <div> Aurora Park  </div>
+            <div> 
+                <h1>  Aurora Park      </h1> 
+           {/* <Link to='/Theme/1'> <Image className='fun1' src="../../Images/HomeCannoeing.png" alt="cannoeing"/> </Link> */}
+            <img className='Home-Image' src={PICNIC_IMAGE} alt="picnic"/> <br></br>
+            <img className='Home-Image' src={PLAY_IMAGE} alt="play"/> <br></br>
+            <img className='Home-Image' src={CANNOE_IMAGE} alt="picnic"/> <br></br>
+            <img className='Home-Image' src={HIKE_IMAGE} alt="picnic"/> <br></br>
+
+            
+            </div>
         )
     }
 }
